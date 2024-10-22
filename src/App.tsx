7a018a6da34ca7367d7c0e0aa8df104896a6d049
chla <hardrockhodl@protonmail.com>
2024-10-22 20:04:58 +0200

    sdsd

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="app-container">
       <div className="card">
-        <h1 className="title">One-Time File Share</h1>
+        <h1 className="title">fil4r</h1>
         <div className="tab-buttons">
           <button
             className={`tab-button ${activeTab === 'upload' ? 'active' : ''}`}
